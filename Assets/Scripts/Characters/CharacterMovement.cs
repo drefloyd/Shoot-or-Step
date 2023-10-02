5c08b87d35e3c26b54b34ae70516f16093f76a76
brhagans <43867067+brhagans@users.noreply.github.com>
2023-10-02 13:16:06 -0400

    Added shooting mechanic to combined player script

--- Assets/Scripts/Characters/CharacterMovement.cs
+++ Assets/Scripts/Characters/CharacterMovement.cs
@@ -3,10 +3,17 @@ using System.Collections.Generic;
 using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 using UnityEngine.UI;
+using static Shooting;
+using static UnityEditor.Experimental.GraphView.GraphView;
 
 public class CharacterMovement : MonoBehaviour
 {
-    public GameObject player;  
+    public GameObject player;
+
+    public ShootingDirection direction = ShootingDirection.East;    //BH This is the initial direction p1 is facing
+    public Transform BulletSpawnPoint;
+    public GameObject BulletPrefab;
+    public float BulletSpeed = 10;
 
     private void Update()
     {
@@ -19,22 +26,52 @@ public class CharacterMovement : MonoBehaviour
                 if (Input.GetKeyDown(KeyCode.LeftArrow))
                 {
                     player.transform.rotation = Quaternion.Euler(0, 0, 180); // Face west
+                    this.direction = ShootingDirection.West;
+                    BulletSpawnPoint.transform.rotation = Quaternion.Euler(0, 0, 180); // Face west
                     --DiceRoll.numMoves;
                 }
                 else if (Input.GetKeyDown(KeyCode.RightArrow))
                 {
                     player.transform.rotation = Quaternion.Euler(0, 0, 0); // Face east
+                    this.direction = ShootingDirection.East;
+                    BulletSpawnPoint.transform.rotation = Quaternion.Euler(0, 0, 0); // Face east
                     --DiceRoll.numMoves;
                 }
 
                 else if (Input.GetKeyDown(KeyCode.DownArrow))
                 {
                     player.transform.rotation = Quaternion.Euler(0, 0, 270); // Face south
+                    this.direction = ShootingDirection.East;
+                    BulletSpawnPoint.transform.rotation = Quaternion.Euler(0, 0, 270); // Face south
                     --DiceRoll.numMoves;
                 }
                 else if (Input.GetKeyDown(KeyCode.UpArrow))
                 {
                     player.transform.rotation = Quaternion.Euler(0, 0, 90); // Face north
+                    this.direction = ShootingDirection.East;
+                    BulletSpawnPoint.transform.rotation = Quaternion.Euler(0, 0, 90); // Face north
+                    --DiceRoll.numMoves;
+                }
+                //BH Space bar to shoot for now. Should make this a button later
+                else if (Input.GetKeyDown(KeyCode.Space))
+                {
+                    var bullet = Instantiate(BulletPrefab, BulletSpawnPoint.position, BulletSpawnPoint.rotation);
+                    if (this.direction == ShootingDirection.West)
+                    {
+                        bullet.GetComponent<Rigidbody2D>().velocity = BulletSpawnPoint.right * BulletSpeed;
+                    }
+                    else if (this.direction == ShootingDirection.East)
+                    {
+                        bullet.GetComponent<Rigidbody2D>().velocity = BulletSpawnPoint.right * BulletSpeed;
+                    }
+                    else if (this.direction == ShootingDirection.North)
+                    {
+                        bullet.GetComponent<Rigidbody2D>().velocity = BulletSpawnPoint.right * BulletSpeed;
+                    }
+                    else if (this.direction == ShootingDirection.South)
+                    {
+                        bullet.GetComponent<Rigidbody2D>().velocity = BulletSpawnPoint.right * BulletSpeed;
+                    }
                     --DiceRoll.numMoves;
                 }
             }
@@ -48,22 +85,52 @@ public class CharacterMovement : MonoBehaviour
                 if (Input.GetKeyDown(KeyCode.LeftArrow))
                 {
                     player.transform.rotation = Quaternion.Euler(0, 0, 0); // Face west
+                    this.direction = ShootingDirection.West;
+                    BulletSpawnPoint.transform.rotation = Quaternion.Euler(0, 0, 0); // Face west
                     --DiceRoll.numMoves;
                 }
                 else if (Input.GetKeyDown(KeyCode.RightArrow))
                 {
                     player.transform.rotation = Quaternion.Euler(0, 0, 180); // Face east
+                    this.direction = ShootingDirection.East;
+                    BulletSpawnPoint.transform.rotation = Quaternion.Euler(0, 0, 180);
                     --DiceRoll.numMoves;
                 }
 
                 else if (Input.GetKeyDown(KeyCode.DownArrow))
                 {
                     player.transform.rotation = Quaternion.Euler(0, 0, 90); // Face south
+                    this.direction = ShootingDirection.East;
+                    BulletSpawnPoint.transform.rotation = Quaternion.Euler(0, 0, 90); // Face south
                     --DiceRoll.numMoves;
                 }
                 else if (Input.GetKeyDown(KeyCode.UpArrow))
                 {
                     player.transform.rotation = Quaternion.Euler(0, 0, 270); // Face north
+                    this.direction = ShootingDirection.East;
+                    BulletSpawnPoint.transform.rotation = Quaternion.Euler(0, 0, 270); // Face north
+                    --DiceRoll.numMoves;
+                }
+                //BH Space bar to shoot for now. Should make this a button later
+                else if (Input.GetKeyDown(KeyCode.Space))
+                {
+                    var bullet = Instantiate(BulletPrefab, BulletSpawnPoint.position, BulletSpawnPoint.rotation);
+                    if (this.direction == ShootingDirection.West)
+                    {
+                        bullet.GetComponent<Rigidbody2D>().velocity = -BulletSpawnPoint.right * BulletSpeed;
+                    }
+                    else if (this.direction == ShootingDirection.East)
+                    {
+                        bullet.GetComponent<Rigidbody2D>().velocity = -BulletSpawnPoint.right * BulletSpeed;
+                    }
+                    else if (this.direction == ShootingDirection.North)
+                    {
+                        bullet.GetComponent<Rigidbody2D>().velocity = -BulletSpawnPoint.right * BulletSpeed;
+                    }
+                    else if (this.direction == ShootingDirection.South)
+                    {
+                        bullet.GetComponent<Rigidbody2D>().velocity = -BulletSpawnPoint.right * BulletSpeed;
+                    }
                     --DiceRoll.numMoves;
                 }
             }
---
+++ Assets/Scripts/Characters/GunScript.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunScript : MonoBehaviour
+{
+    public Transform BulletSpawnPoint;
+    public GameObject BulletPrefab;
+    public float BulletSpeed = 10;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            var bullet = Instantiate(BulletPrefab, BulletSpawnPoint.position, BulletSpawnPoint.rotation);
+            bullet.GetComponent<Rigidbody2D>().velocity = BulletSpawnPoint.up * BulletSpeed;
+        }
+    }
+}
