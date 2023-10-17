d7cc8b722bc312eab55bb9efed606bbe9a177ca9
MohZaatari <91998180+MohZaatari@users.noreply.github.com>
2023-10-17 15:55:53 -0400

    Added a new map and start randomly selects one

--- Assets/Scripts/Main Menu/MainMenu.cs
+++ Assets/Scripts/Main Menu/MainMenu.cs
@@ -7,7 +7,15 @@ public class MainMenu : MonoBehaviour
     // Start is called before the first frame update
    public void PlayGame()
     {
-        SceneManager.LoadSceneAsync(1); 
+        System.Random rnd = new System.Random();
+        if (rnd.Next(0, 2) == 0)
+        {
+            SceneManager.LoadSceneAsync(1);
+        }
+        else
+        {
+            SceneManager.LoadSceneAsync(3);
+        }
     }
     public void MainScreen()
     {
