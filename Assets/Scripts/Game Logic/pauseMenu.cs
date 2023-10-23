53cf155770579d4ee354acc492625ac6dac353fd
Meaad Hassan <meaadhassan09@gmail.com>
2023-10-22 21:18:55 -0400

    added pause menu and cleaned up UI

---
+++ Assets/Scripts/Game Logic/pauseMenu.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class pauseMenu : MonoBehaviour
+{
+    public static bool gameisPaused = false;
+    public GameObject pauseMenuUI;
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(gameisPaused)
+            {
+                Resume();
+
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale =1f;
+        gameisPaused=false;
+    }
+    void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale =0f;
+        gameisPaused=true;
+    }
+}
